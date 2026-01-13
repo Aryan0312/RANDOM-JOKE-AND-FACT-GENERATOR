c563b7c884e88d3d9a8aafe8907c315bae5f8dc4
Aryan0312 <aryan26zzz@gmail.com>
2026-01-13 09:30:21 +0530

    public files update

--- random jokes and facts generator/src/server.js
+++ random jokes and facts generator/src/server.js
@@ -1,13 +1,18 @@
 import ExpressError from "./utils/ExpressError.js";
 import express from "express";
 import wrapAsync from "./utils/wrapAsync.js";
+import path from "path";
+import { fileURLToPath } from "url";
+
 
 const app = express();
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 app.set("view engine", "ejs");
 app.set("views", "../src/views");
-app.use(express.static("public"));
-
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/",wrapAsync(async(req,res)=>{
     res.render("index.ejs")
