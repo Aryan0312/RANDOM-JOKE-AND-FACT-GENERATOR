f4bea5afdcb8ec8ba3d3bdca67eb83e8503eba01
Aryan0312 <aryan26zzz@gmail.com>
2026-01-21 16:32:08 +0530

    changes

--- random jokes and facts generator/src/server.js
+++ random jokes and facts generator/src/server.js
@@ -12,7 +12,11 @@ const __dirname = path.dirname(__filename);
 
 app.set("view engine", "ejs");
 app.set("views", "../src/views");
-app.use(express.static(path.join(__dirname, "public")));
+
+
+const publicPath = path.join(__dirname, "public");
+app.use("/jokes", express.static(publicPath));
+
 
 app.get("/jokes",wrapAsync(async(req,res)=>{
     res.render("index.ejs")
